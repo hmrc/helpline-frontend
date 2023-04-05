24f476946adf7a7d6877b2ea6511a4f14aff7d31
adamliversidge <9948188+adamliversidge@users.noreply.github.com>
2023-04-05 16:25:34 +0100

    VER-3367 [AL] helpline-frontend (FE) implement CSP configuration from bootstrap-play

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "6.3.0-play-28",
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28" % "7.15.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc"         % "7.3.0-play-28",
     "com.beachape"          %% "enumeratum"                 % "1.7.0"
   )
 
   val test = Seq(
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % Test,
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.3.0"              % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.15.0"              % Test,
     "uk.gov.hmrc"             %% "service-integration-test" % "1.3.0-play-28"      % IntegrationTest,
     "org.jsoup"               %  "jsoup"                    % "1.13.1"             % Test,
     "org.scalamock"           %% "scalamock"                % "5.1.0"              % Test,
