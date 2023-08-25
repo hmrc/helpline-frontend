c82c810d55491e62d13e964adfae4df32327c4dc
woollett-r <55831974+woollett-r@users.noreply.github.com>
2023-08-25 15:11:20 +0100

    VER-3778 Decommission service-integration-test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -14,12 +14,11 @@ object AppDependencies {
   val test = Seq(
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % Test,
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapPlayVerson  % Test,
-    "uk.gov.hmrc"             %% "service-integration-test" % "1.3.0-play-28"      % IntegrationTest,
     "org.jsoup"               %  "jsoup"                    % "1.13.1"             % Test,
     "org.scalamock"           %% "scalamock"                % "5.1.0"              % Test,
     "org.mockito"             % "mockito-core"              % "3.8.0"              % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.5"              % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"             % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"              % "test, it"
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"              % "test, it"
   )
 }
