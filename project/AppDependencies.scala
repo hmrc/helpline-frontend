1f0e8f0ac8043fbf2b843e74af802cc7dd70aca3
Will Davidson <will@davidsondigital.uk>
2024-03-06 14:52:52 +0000

    VER-4330: Update test dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,9 +16,9 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapPlayVerson  % Test,
     "org.jsoup"               %  "jsoup"                    % "1.17.2"             % Test,
     "org.scalamock"           %% "scalamock"                % "5.2.0"              % Test,
-    "org.mockito"             %  "mockito-core"             % "3.12.4"             % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.9"              % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8"             % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"              % "test, it"
+    "org.mockito"             %  "mockito-core"             % "5.11.0"             % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.17"              % "test, it",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8"             % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "7.0.1"              % "test, it"
   )
 }
