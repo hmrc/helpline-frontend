67805a25d2d4213a756e26e856bd7ea563111892
sraspin <14580184+sraspin@users.noreply.github.com>
2025-04-04 17:01:44 +0100

    [VER-5441] - pr bot updates

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ import uk.gov.hmrc.DefaultBuildSettings
 val appName = "helpline-frontend"
 
 ThisBuild / majorVersion := 1
-ThisBuild / scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "2.13.16"
 
 lazy val microservice = Project(appName, file("."))
   .enablePlugins(play.sbt.PlayScala, SbtDistributablesPlugin)
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlayVerson = "9.6.0"
+  val bootstrapPlayVerson = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapPlayVerson,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.8.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.13.0",
     "com.beachape"          %% "enumeratum"                 % "1.7.5",
     "commons-codec"         % "commons-codec"               % "1.17.1"
 
